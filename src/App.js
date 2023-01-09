919b79686318063929d3ed93e9d23a7196131294
Festus Kiprop <104489016+festus971@users.noreply.github.com>
2023-01-09 22:25:51 +0300

    updating app js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Home from "./components/Home"
+import Login from './components/Login';
+import About from './components/About';
+import Signup from './components/Signup';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Home/>
+      <Login/>
+      <About/>
+      <Signup/>
     </div>
   );
 }
