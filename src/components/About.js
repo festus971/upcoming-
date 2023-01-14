d79b21b32559d5203927c326211be4697e2ec6bd
Festus Kiprop <104489016+festus971@users.noreply.github.com>
2023-01-14 23:32:28 +0300

    updating descriptions

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Home from "./Home";
 
-function About() {
+function About({bio, links}) {
     return (
     <div>
-        About Upcoming
+        <h2>About me</h2>
+        {bio && bio.lenght >1 ? <p>{bio}</p> : null}
+        <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />"
+        <Home github={links.github} linkden={links.linkden}/>
+        
     </div>
     )
 }
